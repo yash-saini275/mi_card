f522b1df0611392a507a68af9f5bb4acbdf0b02a
Yash Saini <yash.saini275@gmail.com>
2020-02-09 13:45:40 +0530

    final_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,99 @@
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Layout-Challenge-Specs.png'),
+              ),
+
+              Text(
+                'Yash Saini',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                ),
+              ),
+
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                //padding: EdgeInsets.all(10.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 798 213 9170',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'SansSourcePro',
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                //padding: EdgeInsets.all(10.0),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'yash.saini375@gmail.com  ',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: 'SansSourcePro',
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
